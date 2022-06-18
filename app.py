d3e2231a2241851f1ea427a64eff2f1a594771a7
javierpb275 <javierpb275@gmail.com>
2022-06-18 16:17:43 +0200

    stores api

---
+++ app.py
@@ -0,0 +1,34 @@
+from os import environ 
+from dotenv import load_dotenv
+from flask import Flask
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
+
+from resources.user import UserRegister, User, UserLogin
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+
+app = Flask(__name__)
+
+load_dotenv('.env')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_SECRET_KEY'] = environ.get('JWT_SECRET_KEY')
+
+db = SQLAlchemy(app)
+
+jwt = JWTManager(app) 
+
+api = Api(app)
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
---
+++ resources/user.py
@@ -0,0 +1,63 @@
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+from flask_jwt_extended import create_access_token, create_refresh_token
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('username',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('password',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+
+
+class UserRegister(Resource):
+
+    def post(self):
+        data = _user_parser.parse_args()
+
+        if UserModel.find_by_username(data['username']):
+            return {"message": "A user with that username already exists"}, 400
+
+        user = UserModel(**data)
+        user.save_to_db()
+
+        return {"message": "User created successfully."}, 201
+
+
+class User(Resource):
+
+    @classmethod
+    def get(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User not found'}, 404
+        return user.json()
+
+    @classmethod
+    def delete(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User not found'}, 404
+        user.delete_from_db()
+        return {'message': 'User deleted'}, 200
+
+
+class UserLogin(Resource):
+
+    @classmethod
+    def post(cls):
+        data = _user_parser.parse_args()
+        user = UserModel.find_by_username(data['username'])
+        if user and user.password == data['password']:
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return {
+                'access_token': access_token,
+                'refresh_token': refresh_token
+            }, 200
+        return {'message': 'Invalid credentials'}, 401
